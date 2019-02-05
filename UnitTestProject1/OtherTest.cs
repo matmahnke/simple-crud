6a13205cd5b3e2989059d453c9a033f205c2701a
matheus mahnke <matheusmahnke@gmail.com>
2019-02-05 15:23:00 -0200

    voltando test

--- UnitTestProject1/OtherTest.cs
+++ UnitTestProject1/OtherTest.cs
@@ -8,7 +8,7 @@ namespace FunctionalTests
     [TestClass]
     public class OtherTest
     {
-        [TestMethod, Ignore]
+        [TestMethod]
         public void testExample()
         {
             Assert.IsTrue(true);
